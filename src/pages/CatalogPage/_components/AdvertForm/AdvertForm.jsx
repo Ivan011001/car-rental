5ce5184d922c4699feec362f24cb9850fc6e2bd7
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2024-02-03 18:24:21 +0200

    created form mockup

--- src/pages/CatalogPage/_components/AdvertForm/AdvertForm.jsx
+++ src/pages/CatalogPage/_components/AdvertForm/AdvertForm.jsx
@@ -1,5 +1,43 @@
+import Button from "@/components/ui/Button";
+
+import {
+  StyledAdvertForm,
+  StyledAdvertFormItemWrapper,
+  StyledAdvertFormLabel,
+  StyledAvdretFormInput,
+} from "./AdvertForm.styled";
+
 const AdvertForm = () => {
-  return <div>AdvertForm</div>;
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("hi");
+  };
+
+  return (
+    <StyledAdvertForm onSubmit={onFormSubmit}>
+      <StyledAdvertFormItemWrapper>
+        <StyledAdvertFormLabel htmlFor="car_brand">Car Brand</StyledAdvertFormLabel>
+        <StyledAvdretFormInput placeholder="Enter the text" id="car_brand" />
+      </StyledAdvertFormItemWrapper>
+
+      <StyledAdvertFormItemWrapper>
+        <StyledAdvertFormLabel htmlFor="car_price">Price/ 1 hour</StyledAdvertFormLabel>
+        <StyledAvdretFormInput placeholder="To $" id="car_price" />
+      </StyledAdvertFormItemWrapper>
+
+      <StyledAdvertFormItemWrapper>
+        <StyledAdvertFormLabel htmlFor="car_mileage">
+          Сar mileage / km
+        </StyledAdvertFormLabel>
+        <StyledAvdretFormInput placeholder="Enter the text" id="car_mileage" />
+      </StyledAdvertFormItemWrapper>
+
+      <Button padY={14} padX={44}>
+        Search
+      </Button>
+    </StyledAdvertForm>
+  );
 };
 
 export default AdvertForm;
--- src/pages/CatalogPage/_components/AdvertForm/AdvertForm.styled.js
+++ src/pages/CatalogPage/_components/AdvertForm/AdvertForm.styled.js
@@ -1,1 +1,41 @@
-export {};
+import styled from "styled-components";
+
+export const StyledAdvertForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  gap: 18px;
+`;
+
+export const StyledAdvertFormItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  justify-content: flex-start;
+`;
+
+export const StyledAdvertFormLabel = styled.label`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 129%;
+`;
+
+export const StyledAvdretFormInput = styled.input`
+  border-radius: 14px;
+  border: none;
+  padding: 14px 89px 14px 18px;
+  background: #f7f7fb;
+
+  /* transition: var(--transition); */
+
+  &::placeholder {
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 111%;
+    color: #121417;
+  }
+
+  &:focus {
+    outline: 1px solid var(--accent-color);
+  }
+`;
